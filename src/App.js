0486ca78a86d5890948e3a22390486082ca5115e
Ganesh Dhakarke <99081351+ganeshhd2922@users.noreply.github.com>
2023-07-22 20:16:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+import './App.css';
+import Demo from './Demo';
+function App() {
+  return (
+    <div className="App">
+      <Demo/>
+    </div>
+  );
+}
+
+export default App;
